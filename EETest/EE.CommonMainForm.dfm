d56e4f0c7286748bf1a5bd3592c762f8f5a37e86
7744081@gmail.com <Valeriy-Egorov>
2020-05-21 01:31:58 +0300

    full2

--- EETest/EE.CommonMainForm.dfm
+++ EETest/EE.CommonMainForm.dfm
@@ -13,4 +13,13 @@ object EECommonMainFrm: TEECommonMainFrm
   OldCreateOrder = False
   PixelsPerInch = 96
   TextHeight = 13
+  object Button1: TButton
+    Left = 24
+    Top = 20
+    Width = 75
+    Height = 25
+    Caption = 'Button1'
+    TabOrder = 0
+    OnClick = Button1Click
+  end
 end
--- EETest/EE.CommonMainForm.pas
+++ EETest/EE.CommonMainForm.pas
@@ -4,12 +4,16 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
 
 type
   TEECommonMainFrm = class(TForm)
+    Button1: TButton;
+    procedure Button1Click(Sender: TObject);
    private
+
    public
+
     end;
 
 var
@@ -20,6 +24,11 @@ implementation
 {$R *.dfm}
 
 uses EE.Constants;
+
+procedure TEECommonMainFrm.Button1Click(Sender: TObject);
+begin
+//
+end;
 
 end.
 
